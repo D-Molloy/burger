a3b8cc05019ef424b04bee727556c784b4bf000f
Denis Molloy <denis.molloy@gmail.com>
2017-10-01 23:15:52 -0400

    initial commit

---
+++ seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Nookie Supreme Burger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burgermaster', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Freddie's Original Double", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Sodalicious Burger", 1);
