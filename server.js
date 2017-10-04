7fde58e4807e2689990261bd315bb67ed174814a
Denis Molloy <denis.molloy@gmail.com>
2017-10-04 13:13:28 -0400

    updates

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "Dipshit12!!",
     database: "burgers_db"
   });
 }
