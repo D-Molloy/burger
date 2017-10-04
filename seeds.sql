ffe4eaa1a48d6cc3362af07eaa4a6f0e2f8afd63
Denis Molloy <denis.molloy@gmail.com>
2017-10-04 15:43:22 -0400

    updating seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -2,3 +2,5 @@ INSERT INTO burgers (burger_name, devoured) VALUES ('Nookie Supreme Burger', 0);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Burgermaster', 0);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Freddie's Original Double", 1);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Sodalicious Burger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Andre's Famous Bacon Triple Stack", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", 0);
